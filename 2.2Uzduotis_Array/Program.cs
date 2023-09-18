d8d5b5f168c9c876bce70da43f4ef97e0ddc2ec9
Laura <Laura@DESKTOP-BESSB6E>
2023-09-18 23:37:35 +0300

    Add project files.

---
+++ 2.2Uzduotis_Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _2._2Uzduotis_Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            Console.WriteLine(GetFirstLetter(sentence));
+        }
+        public static char GetFirstLetter (string sentence)
+        {
+            char[] chars = sentence.ToCharArray();
+            return chars[0];
+        }
+    } 
+}
+
+
